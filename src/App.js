ef305f860cd7ed2d8a2d9d1ceeddacd965f767fe
shoukat malik <shoukatmalik.dev@gmail.com>
2023-08-08 13:21:31 -0700

    Adding my files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <div className="App">
+     <Layout/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Card(props) {
+
+  return (
+         <div>
+            <div className="card" style={{'width':'18rem'}}>
+            <img src={props.imageUrl} alt="BigCo Inc. logo"/>
+            <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">{props.disc}</p>
+            <a href="#" className="btn btn-primary">{props.btnTitle}</a>
+            </div>
+            
+          </div>
+        </div>
+  )
+}
