f0ee786ba339c9aecd24a7dd9c9e96ddf17657c2
shoukat malik <shoukatmalik.dev@gmail.com>
2023-08-09 12:59:00 -0700

    add changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import Card from './components/Card';
-import Layout from './components/Layout';
+import logo from "./logo.svg";
+import "./App.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <div className="App">
-     <Layout/>
+      <BrowserRouter>
+        <Routes>
+            <Route index element={<Navbar />} /> 
+            <Route path="/" element={<Home />}>
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+          
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Slide.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// export default function slide(props) {
+//   return (
+//     <div>
+//       <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+//   <div className="carousel-inner">
+//     <div className="carousel-item active">
+//       <img src={props.imageUrl} alt="BigCo Inc. logo"/>
+//     </div>
+//     <div className="carousel-item">
+//       <img src={props.imageUrl} alt="BigCo Inc. logo"/>
+//     </div>
+//     <div className="carousel-item">
+//       <img src={props.imageUrl} alt="BigCo Inc. logo"/>
+//     </div>
+//   </div>
+// </div>
+//     </div>
+//   )
+// }
